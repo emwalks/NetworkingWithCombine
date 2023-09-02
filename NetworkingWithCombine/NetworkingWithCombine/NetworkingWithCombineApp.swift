fe0a1e8e6073d5c1105f6c89c68bd652ffa0db2c
Emma Walker <walkere@tcd.ie>
2023-09-02 15:26:25 +0100

    first commit

---
+++ NetworkingWithCombine/NetworkingWithCombine/NetworkingWithCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingWithCombineApp.swift
+//  NetworkingWithCombine
+//
+//  Created by Emma Walker - TVandMobile Platforms - Core Engineering on 02/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct NetworkingWithCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
